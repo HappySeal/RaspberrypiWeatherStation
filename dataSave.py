fbbdb99fea3b456aa8809db87a480f96a23b3d96
HappySeal <cafer15165@gmail.com>
2017-06-02 14:37:16 +0300

    V 0.0.9

--- dataSave.py
+++ dataSave.py
@@ -17,7 +17,7 @@ now = hourMinute()
 while True:
         sensor = BMP180.BMP085()
         nem,sic = DHT.read_retry(11,4)
-        if  hourMinute() - now == 10:
+        if  hourMinute() - now == 5:
                 now = hourMinute()
                 cursor.execute("INSERT INTO Hava_Durumu (tarih,sic,nem,basinc) VALUES (?,?,?,?)",(Now(),sic,nem,sensor.read_pressure()))
         else:
@@ -25,7 +25,7 @@ while True:
                 print("Sicaklik BMP : {0:0.2f} *C".format(sensor.read_temperature()))
                 print("Basinc BMP : {0:0.2f} Pa".format(sensor.read_pressure()))
                 print("Rakim BMP : {0:02f} m".format(sensor.read_altitude()))
-                print("Deniz Seviyesi Basinci BMP : {0:0.2f} Pa".format(sensor.read_sealevel_pressure()))
+                print("Deniz Seviyesi Basinci BMP : {0} mB".format(sensor.read_sealevel_pressure()))
                 print("Sicaklik DHT : {0}".format(sic))
                 print("Nem DHT : {0}".format(nem))
 
